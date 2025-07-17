556d197b131081b231c969c59b18e120f45c0ca1
AritzSanz <aritz.s@hotmail.es>
2025-07-17 10:03:19 +0200

    Cambio parse numero

--- DiaSemana/Program.cs
+++ DiaSemana/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -10,40 +12,37 @@
             Console.WriteLine(value: "Introduce un numero del 1 al 7 ( o 0 para salir):");
             
             texto = Console.ReadLine();
-            dia = Int32.Parse(texto);
+            bool isParsedSuccessfully = Int32.TryParse(texto, out dia);
 
-            switch (dia)
-            {
+            switch (dia) {
                 case 0:
                     Console.WriteLine("¡Hasta luego!");
                     break;
                 case 1:
                     Console.WriteLine("Lunes");
-                    continue;
+                    break;
                 case 2:
                     Console.WriteLine("Martes");
-                    continue;
+                    break;
                 case 3:
                     Console.WriteLine("Miércoles");
-                    continue;
+                    break;
                 case 4:
                     Console.WriteLine("Jueves");
-                    continue;
+                    break;
                 case 5:
                     Console.WriteLine("Viernes");
-                    continue;
+                    break;
                 case 6:
                     Console.WriteLine("Sabado");
-                    continue;
+                    break;
                 case 7:
                     Console.WriteLine("Domingo");
-                    continue;
+                    break;
                 default:
                     Console.WriteLine("Valor no válido, intenta de nuevo.");
-                    continue;
+                    break;
             }
-
-
         }
     }
 }
